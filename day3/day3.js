6a35dd096cf1c4668d3d834804760957e9e5d427
SirajMia <sirajulshuvo08@gmail.com>
2024-03-31 18:39:12 +0600

    day3problem adding

---
+++ day3/day3.js
@@ -0,0 +1,13 @@
+// Write a function to sort an array of number in an asecnding order..
+// todo Requirement's
+// the should take an array of number as a input.
+// it should reuturn a new array with the number sorted in ascending order.
+// The original array should remai unchanged.
+// You are not allowed to use the  bulit-in sort()mathod..
+// Example console.log(sortAscendingOrder([1,3,2,33,22])) // output [1,2,2,22,33];
+
+function sortAscendingOrder(inputArr) {
+    const arr = inputArr.sort((a,b) => a - b)
+    return arr;
+}
+console.log(sortAscendingOrder([1,3,2,33,22,4,5]))
