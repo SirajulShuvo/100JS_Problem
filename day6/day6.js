31ab3c6be57f83f088ef83f65ade4ff2b07b0d26
SirajMia <sirajulshuvo08@gmail.com>
2024-04-03 17:09:30 +0600

    add day6

---
+++ day6/day6.js
@@ -0,0 +1,14 @@
+//*.................................................
+//* Js interview Question
+//*.................................................
+// write a function factorail that takes a non-negative integer num as input and returns it's factorial.
+//The factorial of a non-negative integer n,denote as n!,is the product of all positive interger less than or equal to n.factorial of 0 define4d as 1.
+
+function factorial(n){
+    let factorial = 1;
+    for(let i = n; i >= 1 ; i--){
+        factorial *= i;
+    }
+    return factorial;
+}
+console.log(factorial(4));
